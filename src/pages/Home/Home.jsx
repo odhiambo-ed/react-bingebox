8385f7b963637c34d2c4a803345a5ebeb88b4dfe
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-01 15:34:34 +0300

    edit home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,15 @@ import Navbar from '../../components/Navbar/Navbar'
 
 function Home() {
   return (
-      <div>
+      <div className='home'>
           <Navbar />
+          <div className="hero">
+              <img src="" alt="" />
+              <div className="hero-content">
+                  <img src="" alt="" />
+                  <p>Discover his ties to a secret ancient order, a young man living in a modern Istabul embarks on a quest to save the city from an immortal enemy. </p>
+              </div>
+          </div>
     </div>
   )
 }
