9fa83f42d52fec9605a2f010ab593af454aa89ba
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-01 19:25:14 +0300

    add hero content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,17 +4,21 @@ import HeroTitle from '../../assets/hero_title.png';
 
 function Home() {
   return (
-      <div className='home'>
-          <Navbar />
-          <div className="hero">
-              <img src={HeroBanner} alt="" />
-              <div className="hero-content">
-                  <img src={HeroTitle} alt="" className='content-image' />
-                  <p>Discover his ties to a secret ancient order, a young man living in a modern Istabul embarks on a quest to save the city from an immortal enemy. </p>
-              </div>
-          </div>
+    <div className="home">
+      <Navbar />
+      <div className="hero">
+        <img src={HeroBanner} alt="" className="banner-image" />
+        <div className="hero-content">
+          <img src={HeroTitle} alt="" className="content-image" />
+          <p>
+            Discover his ties to a secret ancient order, a young man living in a
+            modern Istabul embarks on a quest to save the city from an immortal
+            enemy.{" "}
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
