c35072a9cac51fe798c356f79c91894ffa5a5b84
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-01 21:19:22 +0300

    add hero buttons

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import Navbar from '../../components/Navbar/Navbar'
 import HeroBanner from '../../assets/hero_banner.jpg';
 import HeroTitle from '../../assets/hero_title.png';
 import './Home.css';
+import Play from '../../assets/play_icon.png';
+import Info from '../../assets/info_icon.png';
 
 function Home() {
   return (
@@ -15,7 +17,11 @@ function Home() {
             Discover his ties to a secret ancient order, a young man living in a
             modern Istabul embarks on a quest to save the city from an immortal
             enemy.
-          </p>
+                  </p>
+                  <div className="hero-button">
+                      <button className='btn'><img src={Play} alt="Play" />Play</button>
+                      <button className='btn dark-btn'><img src={Info} alt="Info" />More Info</button>
+                  </div>
         </div>
       </div>
     </div>
