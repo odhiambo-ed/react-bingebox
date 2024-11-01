8f13b6a8ce9d65d75d43d5742aa06938ca72a4ab
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-01 11:51:53 +0300

    add logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,17 @@
-
+import Logo from '../../assets/logo.png';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+      <div className="navbar">
+          <div className="navbar-left">
+              <img src={Logo} alt="Logo" />
+              <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+              </ul>
+          </div>
+    </div>
   )
 }
 
