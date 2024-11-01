52827fc8d170a04891860ff6c956ae402cb50d67
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-01 12:01:53 +0300

    create navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import Logo from '../../assets/logo.png';
+import SearchIcon from '../../assets/search_icon.svg';
+import BellIcon from '../../assets/bell_icon.svg';
 
 function Navbar() {
   return (
@@ -7,9 +9,20 @@ function Navbar() {
               <img src={Logo} alt="Logo" />
               <ul>
                   <li>Home</li>
-                  <li>About</li>
-                  <li>Contact</li>
+                  <li>Tv Shows</li>
+                  <li>Movies</li>
+                  <li>New & Popular</li>
+                  <li>My List</li>
+                  <li>Browse by Languages</li>
               </ul>
+          </div>
+          <div className="navbar-right">
+              <img src={SearchIcon} alt="Search Icon" className='icons' />
+              <p>Children</p>
+              <img src={BellIcon} alt="Bell Icon" className='icons' />
+              <div className="navbar-profile">
+                  <img src={BellIcon} alt="Bell Icon" className='icons' />
+              </div>
           </div>
     </div>
   )
