42fde6a3bd68985a7411d77b0c26270e01815bd8
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-02 22:37:59 +0300

    add hero button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
-import Navbar from '../../components/Navbar/Navbar'
-import HeroBanner from '../../assets/hero_banner.jpg';
-import HeroTitle from '../../assets/hero_title.png';
-import './Home.css';
-import Play from '../../assets/play_icon.png';
-import Info from '../../assets/info_icon.png';
+import Navbar from "../../components/Navbar/Navbar";
+import HeroBanner from "../../assets/hero_banner.jpg";
+import HeroTitle from "../../assets/hero_title.png";
+import "./Home.css";
+import Play from "../../assets/play_icon.png";
+import Info from "../../assets/info_icon.png";
+import Card from "../../components/Cards/Cards";
 
 function Home() {
   return (
@@ -17,15 +18,22 @@ function Home() {
             Discover his ties to a secret ancient order, a young man living in a
             modern Istabul embarks on a quest to save the city from an immortal
             enemy.
-                  </p>
-                  <div className="hero-button">
-                      <button className='btn'><img src={Play} alt="Play" />Play</button>
-                      <button className='btn dark-btn'><img src={Info} alt="Info" />More Info</button>
-                  </div>
+          </p>
+          <div className="hero-button">
+            <button className="btn">
+              <img src={Play} alt="Play" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={Info} alt="Info" />
+              More Info
+            </button>
+          </div>
+          <Card />
         </div>
       </div>
     </div>
   );
 }
 
-export default Home
+export default Home;
