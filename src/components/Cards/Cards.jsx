5af5f6763afa6d7611b2566eac0459552ab87264
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-02 23:02:48 +0300

    edit error

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,21 +1,23 @@
 import cards_data from "../../assets/cards/Cards_data";
-import './Cards.css'
+import "./Cards.css";
 
-console.log(cards_data)
+console.log(cards_data);
 function Cards() {
   return (
     <div className="cards">
       <h2>Popular on NetFlix</h2>
       <div className="card-list">
-              {cards_data.map((index, card) => {
-                 return <div key={index} className="single-card">
-                      <img src={card.image} alt="" />
-                      <p>{card.image}</p>
-                  </div>
-})}
+        {cards_data.map((card, index) => {
+          return (
+            <div key={index} className="single-card">
+              <img src={card.image} alt={card.name} />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 }
 
-export default Cards
+export default Cards;
