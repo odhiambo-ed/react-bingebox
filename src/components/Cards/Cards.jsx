95a885add19c169a3c965c44fa07ce45b238ff3f
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-02 22:20:36 +0300

    add card components

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Cards() {
+  return (
+      <div className='cards'>
+          <h2>Popular on NetFlix</h2>
+          <div className="card-list">
+              
+          </div>
+    </div>
+  )
+}
+
+export default Cards
