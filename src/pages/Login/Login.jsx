e1a72e9b87fbb85177104c8a528cf7496d6d73ea
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-03 20:12:17 +0300

    add form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,10 +3,27 @@ import Logo from '../../assets/logo.png'
 
 function Login() {
   return (
-      <div className='login'>
-          <img src={ Logo } alt="Logo" />
+    <div className="login">
+      <img src={Logo} alt="Logo" />
+      <div className="form">
+        <h2>Sign Up</h2>
+        <form>
+          <input type="text" placeholder="Your Name" />
+          <input type="text" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Confirm Password" />
+          <button type="submit">Sign Up</button>
+          <div className="form-help">
+                      <div className="remember">
+                          <input type="checkbox" />
+                          <label htmlFor="">Remember Me</label>
+                      </div>
+                      <p>Need Help?</p>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Login
