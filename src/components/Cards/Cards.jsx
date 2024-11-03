256688cece858239c83129b26a1e01ba238aa556
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-03 18:49:43 +0300

    fix proptype error

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,21 +1,26 @@
 import { useEffect, useRef } from "react";
+import PropTypes from "prop-types"; // Import PropTypes
 import cards_data from "../../assets/cards/Cards_data";
 import "./Cards.css";
 
-function Cards() {
-    const cardsRef = useRef();
+function Cards({ title }) {
+  const cardsRef = useRef();
 
-    const handleWheel = (event) => { 
-        event.preventDefault();
-        cardsRef.current.scrollLeft += event.deltaY;
-    }
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
 
-    useEffect(() => {
-        cardsRef.current.addEventListener('wheel', handleWheel)
-    }, [])
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+    return () => {
+      cardsRef.current.removeEventListener("wheel", handleWheel);
+    };
+  }, []);
+
   return (
     <div className="cards">
-      <h2>Popular on NetFlix</h2>
+      <h2>{title ? title : "Popular on NetFlix"}</h2>
       <div className="card-list" ref={cardsRef}>
         {cards_data.map((card, index) => {
           return (
@@ -29,5 +34,10 @@ function Cards() {
     </div>
   );
 }
+
+// Add PropTypes validation
+Cards.propTypes = {
+  title: PropTypes.string,
+};
 
 export default Cards;
