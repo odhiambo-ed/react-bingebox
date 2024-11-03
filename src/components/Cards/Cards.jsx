91d0228ac125ced90235d86a663d6afd2ce5eacf
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-03 23:16:55 +0300

    add fetch function

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -6,16 +6,31 @@ import "./Cards.css";
 function Cards({ title }) {
   const cardsRef = useRef();
 
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZjA0ZGNkMWU3ZDUxN2I1OGUyODQ0ZjY4ZjQzMWMyNSIsIm5iZiI6MTczMDY2NDI4Ny4wNzA4NzM3LCJzdWIiOiI2NTc3MWJmNjRiZmE1NDAwZmU3ZjNmNGMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Kba3JAOu0mAbdNF6fAzcZJHPhnaiq1xNmW64T5VdPCY",
+      },
+    };
+    
+    
   const handleWheel = (event) => {
     event.preventDefault();
     cardsRef.current.scrollLeft += event.deltaY;
   };
 
-  useEffect(() => {
+    useEffect(() => {
+      fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+        options
+      )
+        .then((res) => res.json())
+        .then((res) => )
+          .catch((err) => console.error(err));
+        
     cardsRef.current.addEventListener("wheel", handleWheel);
-    return () => {
-      cardsRef.current.removeEventListener("wheel", handleWheel);
-    };
   }, []);
 
   return (
