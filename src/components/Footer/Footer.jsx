c456ef25aff3567c1514eee341bbdbc85be1077c
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-03 19:00:44 +0300

    import social images

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,16 @@
 import './Footer.css'
+import Youtube from '../../assets/youtube_icon.png'
+import Facebook from '../../assets/facebook_icon.png'
+import Twitter from '../../assets/twitter_icon.png'
+import Instagram from '../../assets/instagram_icon.png'
 
 function Footer() {
   return (
-    <div>Footer</div>
+      <div className='footer'>
+          <div className="footer-icons">
+            <img src="" alt="" />
+          </div> 
+    </div>
   )
 }
 
