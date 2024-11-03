481f2c83d534683b0b68c089b2c4ea9ae6330c46
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-03 19:18:32 +0300

    add copyright symbol

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,32 +6,30 @@ import Instagram from '../../assets/instagram_icon.png'
 
 function Footer() {
   return (
-      <div className='footer'>
-          <div className="footer-icons">
-              <img src={Youtube} alt="YouTube" />
-              <img src={Facebook} alt="Facebook" />
-              <img src={Twitter} alt="Twitter" />
-              <img src={Instagram} alt="Instagram" />
-          </div> 
-          <ul>
-              <li>Audio Description</li>
-              <li>Help Centre</li>
-              <li>Gift Cards</li>
-              <li>Media Centre</li>
-              <li>Investor Relations</li>
-              <li>Jobs</li>
-              <li>Terms of Use</li>
-              <li>Privacy</li>
-              <li>Legal Notices</li>
-              <li>Cookie Preferences</li>
-              <li>Corporate Information</li>
-              <li>Contact Us</li>
-          </ul>
-          <p className="copyright">
-              1997-2024 NetFlix, Inc.
-          </p>
+    <div className="footer">
+      <div className="footer-icons">
+        <img src={Youtube} alt="YouTube" />
+        <img src={Facebook} alt="Facebook" />
+        <img src={Twitter} alt="Twitter" />
+        <img src={Instagram} alt="Instagram" />
+      </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Centre</li>
+        <li>Gift Cards</li>
+        <li>Media Centre</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notices</li>
+        <li>Cookie Preferences</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className="copyright">&#169;1997-2024 NetFlix, Inc.</p>
     </div>
-  )
+  );
 }
 
 export default Footer
