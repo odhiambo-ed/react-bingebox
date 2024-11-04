28527258d12295c2d837a03984c77fbb133e4b41
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-04 14:38:45 +0300

    edit play route

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/play' element={<Play />} />
+        <Route path='/play/:id' element={<Play />} />
       </Routes>
     </>
   )
