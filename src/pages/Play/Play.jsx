3ce0c7ceab9a5224b0a101024eecd6eb7931091f
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-04 14:32:24 +0300

    add iframe

--- src/pages/Play/Play.jsx
+++ src/pages/Play/Play.jsx
@@ -4,10 +4,19 @@ import Arrow from '../../assets/back_arrow_icon.png'
 
 function Play() {
   return (
-      <div className='player'>
-          <img src={Arrow} alt="Arrow" />
+    <div className="player">
+      <img src={Arrow} alt="Arrow" />
+      <iframe
+        src="https://www.youtube.com/embed/mzs2CP5v6bw" width="90%" height="90%"
+        frameborder="0" title='Trailer' allowFullScreen="true"
+          ></iframe>
+          <div className="player-info">
+              <p>Published Date</p>
+              <p>Name</p>
+              <p>Type</p>
+          </div>
     </div>
-  )
+  );
 }
 
 export default Play
