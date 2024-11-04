20d0e63d1d259c483d58b749666bbdafd5764366
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-11-04 22:00:50 +0300

    edit fetch

--- src/pages/Play/Play.jsx
+++ src/pages/Play/Play.jsx
@@ -1,50 +1,55 @@
-import './Play.css'
-import Arrow from '../../assets/back_arrow_icon.png'
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-
+import "./Play.css";
+import Arrow from "../../assets/back_arrow_icon.png";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Play() {
-    const { id } = useParams();
-    const [apiData, setApiData] = useState([
-      {
-        name: "",
-        published_at: "",
-        type: "",
-      },
-    ]);
+  const { id } = useParams();
+  const [apiData, setApiData] = useState({
+    key: "",
+    name: "",
+    published_at: "",
+    type: "",
+  });
 
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZjA0ZGNkMWU3ZDUxN2I1OGUyODQ0ZjY4ZjQzMWMyNSIsIm5iZiI6MTczMDY2NDI4Ny4wNzA4NzM3LCJzdWIiOiI2NTc3MWJmNjRiZmE1NDAwZmU3ZjNmNGMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Kba3JAOu0mAbdNF6fAzcZJHPhnaiq1xNmW64T5VdPCY",
-      },
-    };
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZjA0ZGNkMWU3ZDUxN2I1OGUyODQ0ZjY4ZjQzMWMyNSIsIm5iZiI6MTczMDY2NDI4Ny4wNzA4NzM3LCJzdWIiOiI2NTc3MWJmNjRiZmE1NDAwZmU3ZjNmNGMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Kba3JAOu0mAbdNF6fAzcZJHPhnaiq1xNmW64T5VdPCY",
+    },
+  };
 
-    useEffect(() => {
-        fetch(
-            `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
-            options
-        )
-            .then((res) => res.json())
-            .then((res) => setApiData(res.results[0]))
-            .catch((err) => console.error(err));
-    }, [id]);
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.results && res.results.length > 0) {
+          setApiData(res.results[0]);
+        }
+      })
+      .catch((err) => console.error(err));
+  }, [id]);
+
   return (
     <div className="player">
       <img src={Arrow} alt="Arrow" />
-      <iframe
-        src={`https://www.youtube.com/embed/${apiData.key}`}
-        width="90%"
-        height="90%"
-              title="Trailer"
-              frameBorder="0"
-        allowFullScreen="true"
-      ></iframe>
+      {apiData.key && (
+        <iframe
+          src={`https://www.youtube.com/embed/${apiData.key}`}
+          width="90%"
+          height="90%"
+          title="Trailer"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      )}
       <div className="player-info">
-        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.published_at.slice(0, 10)}</p>
         <p>{apiData.name}</p>
         <p>{apiData.type}</p>
       </div>
@@ -52,4 +57,4 @@ function Play() {
   );
 }
 
-export default Play
+export default Play;
